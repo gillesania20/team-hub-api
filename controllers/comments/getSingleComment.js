dcec38c31a0f6918ae69a95639dd1364e9dd9c92
gillesania20 <lorenzgillesania20@gmail.com>
2023-08-09 17:18:17 +0800

    modified 'getSingleComment.js' and 'updateComment.js'

--- controllers/comments/getSingleComment.js
+++ controllers/comments/getSingleComment.js
@@ -1,17 +1,13 @@
 import { userFindOne } from './../../models/users/userQueries.js';
-import { postFindOne } from './../../models/posts/postQueries.js';
 import { commentFindOne } from './../../models/comments/commentQueries.js';
 import { validateUsername, validateId } from './../../functions/validation.js';
 const getSingleComment = async (req, res) => {
     const username = req.username;
-    const postID = req.body.postID;
     const commentID = req.params.commentID;
     const validatedUsername = validateUsername(username);
-    let validatedPostId = false;
     let validatedCommentId = false;
     let response = null;
     let findUser = null;
-    let findPost = null;
     let findComment = null;
     if(validatedUsername === false){
         response = {
@@ -28,46 +24,28 @@ const getSingleComment = async (req, res) => {
                 comment: null
             }
         }else{
-            validatedPostId = validateId(postID);
-            if(validatedPostId === false){
+            validatedCommentId = validateId(commentID);
+            if(validatedCommentId === false){
                 response = {
                     status: 400,
-                    message: 'invalid post ID',
+                    message: 'invalid comment ID',
                     comment: null
                 }
             }else{
-                findPost = await postFindOne({_id: postID}, '_id');
-                if(findPost === null){
+                findComment = await commentFindOne(
+                    {_id: commentID, user: findUser._id.toString()},
+                    'body created_at updated_at');
+                if(findComment === null){
                     response = {
                         status: 404,
-                        message: 'post not found',
+                        message: 'comment not found',
                         comment: null
                     }
                 }else{
-                    validatedCommentId = validateId(commentID);
-                    if(validatedCommentId === false){
-                        response = {
-                            status: 400,
-                            message: 'invalid comment ID',
-                            comment: null
-                        }
-                    }else{
-                        findComment = await commentFindOne(
-                            {_id: commentID, user: findUser._id.toString(), post: findPost._id.toString()},
-                            'body created_at updated_at');
-                        if(findComment === null){
-                            response = {
-                                status: 404,
-                                message: 'comment not found',
-                                comment: null
-                            }
-                        }else{
-                            response = {
-                                status: 200,
-                                message: 'comment found',
-                                comment: findComment
-                            }
-                        }
+                    response = {
+                        status: 200,
+                        message: 'comment found',
+                        comment: findComment
                     }
                 }
             }
--- controllers/comments/updateComment.js
+++ controllers/comments/updateComment.js
@@ -1,19 +1,15 @@
 import { userFindOne } from './../../models/users/userQueries.js';
-import { postFindOne } from './../../models/posts/postQueries.js';
 import { commentFindOne, commentUpdateOne } from './../../models/comments/commentQueries.js';
 import { validateUsername, validateId, validateBody } from './../../functions/validation.js';
 const updateComment = async (req, res) => {
     const username = req.username;
-    const postID = req.body.postID;
     const commentID = req.params.commentID;
     const body = req.body.body;
     const validatedUsername = validateUsername(username);
-    let validatedPostId = false;
     let validatedCommentId = false;
     let validatedBody = false;
     let response = null;
     let findUser = null;
-    let findPost = null;
     let findComment = null;
     let update = {}
     if(validatedUsername === false){
@@ -29,53 +25,37 @@ const updateComment = async (req, res) => {
                 message: 'user not found'
             }
         }else{
-            validatedPostId = validateId(postID);
-            if(validatedPostId === false){
+            validatedCommentId = validateId(commentID);
+            if(validatedCommentId === false){
                 response = {
                     status: 400,
-                    message: 'invalid post ID'
+                    message: 'invalid comment ID'
                 }
             }else{
-                findPost = await postFindOne({_id: postID}, '_id');
-                if(findPost === null){
+                findComment = await commentFindOne(
+                    {_id: commentID, user: findUser._id.toString()}, '_id');
+                if(findComment === null){
                     response = {
                         status: 404,
-                        message: 'post not found'
+                        message: 'comment not found'
                     }
                 }else{
-                    validatedCommentId = validateId(commentID);
-                    if(validatedCommentId === false){
+                    if(typeof body !== 'undefined'){
+                        validatedBody = validateBody(body)
+                    }
+                    if(typeof body !== 'undefined' && validatedBody === false){
                         response = {
                             status: 400,
-                            message: 'invalid comment ID'
+                            message: 'invalid body. should start with any character followed by any character or white space. minimum 1 and maximum 500 characters'
                         }
                     }else{
-                        findComment = await commentFindOne(
-                            {_id: commentID, user: findUser._id.toString(), post: findPost._id.toString()}, '_id');
-                        if(findComment === null){
-                            response = {
-                                status: 404,
-                                message: 'comment not found'
-                            }
-                        }else{
-                            if(typeof body !== 'undefined'){
-                                validatedBody = validateBody(body)
-                            }
-                            if(typeof body !== 'undefined' && validatedBody === false){
-                                response = {
-                                    status: 400,
-                                    message: 'invalid body. should start with any character followed by any character or white space. minimum 1 and maximum 500 characters'
-                                }
-                            }else{
-                                if(typeof body !== 'undefined'){
-                                    update.body = body;
-                                }
-                                await commentUpdateOne({_id: findComment._id.toString()}, update);
-                                response = {
-                                    status: 200,
-                                    message: 'updated comment data'
-                                }
-                            }
+                        if(typeof body !== 'undefined'){
+                            update.body = body;
+                        }
+                        await commentUpdateOne({_id: findComment._id.toString()}, update);
+                        response = {
+                            status: 200,
+                            message: 'updated comment data'
                         }
                     }
                 }
