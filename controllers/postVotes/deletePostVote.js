f0857cbef1dbed953dbe3929914d3437a1724519
gillesania20 <lorenzgillesania20@gmail.com>
2023-08-23 16:09:28 +0800

    modified 'deletePostVote.js'

--- controllers/postVotes/deletePostVote.js
+++ controllers/postVotes/deletePostVote.js
@@ -1,14 +1,25 @@
 import { userFindOne } from './../../models/users/userQueries.js';
-import { postVoteFindOne, postVoteDeleteOne } from './../../models/postVotes/postVoteQueries.js';
+import { teamFindOne } from './../../models/teams/teamQueries.js';
+import { postFindOne, postUpdateOne } from './../../models/posts/postQueries.js';
+import { postVoteFindOne, postVoteDeleteOne, postVoteFind } from './../../models/postVotes/postVoteQueries.js';
 import { validateUsername, validateId } from './../../functions/validation.js';
 const deletePostVote = async (req, res) => {
     const username = req.username;
+    const teamID = req.body.teamID;
+    const postID = req.body.postID;
     const postVoteID = req.params.postVoteID;
     const validatedUsername = validateUsername(username);
+    let validatedTeamId = false;
+    let validatedPostId = false;
     let validatedPostVoteId = false;
     let response = null;
     let findUser = null;
+    let findTeam = null;
+    let findPost = null;
     let findPostVote = null;
+    let findPostVotes = null;
+    let likes = 0;
+    let dislikes = 0;
     if(validatedUsername === false){
         response = {
             status: 401,
@@ -22,24 +33,65 @@ const deletePostVote = async (req, res) => {
                 message: 'user not found'
             }
         }else{
-            validatedPostVoteId = validateId(postVoteID);
-            if(validatedPostVoteId === false){
+            validatedTeamId = validateId(teamID);
+            if(validatedTeamId === false){
                 response = {
                     status: 400,
-                    message: 'invalid post-vote ID'
+                    message: 'invalid team ID'
                 }
             }else{
-                findPostVote = await postVoteFindOne({_id: postVoteID, user: findUser._id.toString()}, '_id');
-                if(findPostVote === null){
+                findTeam = await teamFindOne({_id: teamID }, '_id');
+                if(findTeam === null){
                     response = {
                         status: 404,
-                        message: 'post-vote not found'
+                        message: 'team not found'   
                     }
                 }else{
-                    await postVoteDeleteOne({_id: findPostVote._id.toString()});
-                    response = {
-                        status: 200,
-                        message: 'post-vote deleted'
+                    validatedPostId = validateId(postID);
+                    if(validatedPostId === false){
+                        response = {
+                            status: 400,
+                            message: 'invalid post ID'
+                        }
+                    }else{
+                        findPost = await postFindOne({_id: postID}, '_id');
+                        if(findPost === null){
+                            response = {
+                                status: 404,
+                                message: 'post not found'
+                            }
+                        }else{
+                            validatedPostVoteId = validateId(postVoteID);
+                            if(validatedPostVoteId === false){
+                                response = {
+                                    status: 400,
+                                    message: 'invalid post-vote ID'
+                                }
+                            }else{
+                                findPostVote = await postVoteFindOne({_id: postVoteID, user: findUser._id.toString()}, '_id');
+                                if(findPostVote === null){
+                                    response = {
+                                        status: 404,
+                                        message: 'post-vote not found'
+                                    }
+                                }else{
+                                    await postVoteDeleteOne({_id: findPostVote._id.toString()});
+                                    findPostVotes = await postVoteFind({post: findPost._id}, 'vote');
+                                    for(let i = 0; i < findPostVotes.length; i++){
+                                        if(findPostVotes[i].vote === 1){
+                                            likes++;
+                                        }else if(findPostVotes[i].vote === -1){
+                                            dislikes++;
+                                        }
+                                    }
+                                    await postUpdateOne({_id: findPost._id}, {likes, dislikes});
+                                    response = {
+                                        status: 200,
+                                        message: 'post-vote deleted'
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
