2a133809dfabf5f60b7fbd0f45b032e65ec07537
gillesania20 <lorenzgillesania20@gmail.com>
2023-08-31 21:50:28 +0800

    modified 'constants.js'

--- constants.js
+++ constants.js
@@ -3,7 +3,7 @@ const COOKIE_HTTP_ONLY = true;
 const COOKIE_SECURE = (process.env.NODE_ENV === 'production')?true:false;
 const COOKIE_SAME_SITE = (process.env.NODE_ENV === 'production')?'None':false;
 const COOKIE_MAX_AGE = (process.env.NODE_ENV === 'production')?(1000*60*60*24):(1000*60*30);
-const ACCESS_TOKEN_EXPIRES_IN = (process.env.NODE_ENV === 'production')?5:'1h';
+const ACCESS_TOKEN_EXPIRES_IN = (process.env.NODE_ENV === 'production')?5:5;
 const REFRESH_TOKEN_EXPIRES_IN = (process.env.NODE_ENV === 'production')?'24h':'1h';
 export {
     SALT_ROUNDS,
