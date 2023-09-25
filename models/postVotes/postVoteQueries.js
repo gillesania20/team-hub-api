ffead8f5cd98a695ba23187a218f2afb04557a20
gillesania20 <lorenzgillesania20@gmail.com>
2023-09-25 19:05:20 +0800

    DEPLOY TEST 3

--- models/postVotes/postVoteQueries.js
+++ models/postVotes/postVoteQueries.js
@@ -1,4 +1,4 @@
-import PostVote from './PostVote.js';
+import PostVote from './postVote.js';
 const postVoteFindOne = async (conditions, projection, options) => {
     const query = await PostVote.findOne(conditions, projection, options).lean().exec();
     return query;
