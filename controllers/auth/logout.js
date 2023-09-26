a2e5f1eb09cdc26ec5b1025557f59ccdea4d0596
gillesania20 <lorenzgillesania20@gmail.com>
2023-09-26 14:03:44 +0800

    modified 'logout.js'

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -1,5 +1,10 @@
+import { COOKIE_HTTP_ONLY, COOKIE_SECURE, COOKIE_SAME_SITE } from './../../constants.js';
 const logout = (req, res) => {
-    res.clearCookie('jwt');
+    res.clearCookie('jwt', {
+        httpOnly: COOKIE_HTTP_ONLY,
+        secure: COOKIE_SECURE,
+        sameSite: COOKIE_SAME_SITE
+    });
     return res.status(200).json({message: 'logout successful'});
 }
 export default logout;
